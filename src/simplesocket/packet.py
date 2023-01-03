8baa9b6bc12d7886304a344d42ed1bb0836db5e8
bherbruck <brennen.herbruck@gmail.com>
2023-01-03 00:25:21 -0500

    larger packet

--- src/simplesocket/packet.py
+++ src/simplesocket/packet.py
@@ -12,14 +12,20 @@ class Packet:
     The next two bytes are the length of the event.\n
     The rest of the packet is the event and the message.\n
     ```txt
-    +---------------+---------------+----------+----------+
-    | Packet Length | Event Length  | Event    | Message  |
-    | (2 bytes)     | (2 bytes)     | (utf-8)  | (bytes)  |
-    +---------------+---------------+----------+----------+
+    +------------------------------+-------------------+
+    | Header                       | Payload           |
+    | (8 bytes)                    |                   |
+    +---------------+--------------+---------+---------+
+    | Packet Length | Event Length | Event   | Message |
+    | (4 bytes)     | (4 bytes)    | (utf-8) | (bytes) |
+    +---------------+--------------+---------+---------+
     ```
+    TODO: check integrity of packet (could be done with ssl/tls)
+    TODO: packet fragmentation (if we want to send large packets)
     """
 
-    HEADER_LENGTH = 4
+    MAX_PACKET_SIZE = 65535  # (ish?)
+    HEADER_LENGTH = 8
 
     def __init__(self, event: str, message: bytes):
         self.event = event
@@ -35,16 +41,16 @@ class Packet:
             message_bytes = message
         packet_length = len(event_bytes) + len(message_bytes) + Packet.HEADER_LENGTH
         event_length = len(event_bytes)
-        return (
-            struct.pack("!HH", packet_length, event_length)
-            + event_bytes
-            + message_bytes
-        )
+        header = struct.pack("!LL", packet_length, event_length)
+        packet = header + event_bytes + message_bytes
+        return packet
 
     @staticmethod
     def decode(packet: bytes) -> "Packet":
         """Decode a packet into a event and message."""
-        packet_length, event_length = struct.unpack("!HH", packet[:4])
+        packet_length, event_length = struct.unpack(
+            "!LL", packet[: Packet.HEADER_LENGTH]
+        )
         event: bytes = packet[
             Packet.HEADER_LENGTH : Packet.HEADER_LENGTH + event_length
         ].decode("utf-8")
@@ -57,15 +63,15 @@ class Packet:
     def receive(socket: Socket) -> "Packet":
         """Receive a packet from a socket."""
         try:
-            header = socket.recv(4)
+            header = socket.recv(Packet.HEADER_LENGTH)
             if not header or len(header) != Packet.HEADER_LENGTH:
                 return None
-
-            packet_length, event_length = struct.unpack("!HH", header)
+            packet_length, event_length = struct.unpack("!LL", header)
             event = socket.recv(event_length)
             data = socket.recv(packet_length - event_length - Packet.HEADER_LENGTH)
             return Packet.decode(header + event + data)
-        except (ConnectionError, OSError, ValueError):
+        except (ConnectionError, OSError, ValueError) as exception:
+            print(exception)
             return None
 
     @staticmethod
